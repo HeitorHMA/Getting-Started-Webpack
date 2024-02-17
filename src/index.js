49338e13a042e0b24d59510974c29d3c519124a6
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2024-02-17 11:15:21 -0300

    Change Main.js to Bundle.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import _ from 'lodash';
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script.
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+import './style.css';
+
+ function component() {
+   const element = document.createElement('div');
+
+   // Lodash, now imported by this script
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+   return element;
+ }
+
+ document.body.appendChild(component());
